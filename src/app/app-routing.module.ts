09be58b59b394cda77f6ea5346bf9d83b6c88496
Ana Paula Campos <paulinha.vhh@gmail.com>
2023-08-05 22:10:10 -0300

    componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, { path: 'cadastro', loadChildren: () => import('./cadastro/cadastro.module').then(m => m.CadastroModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
